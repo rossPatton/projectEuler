21ea29e1630e4863db1ea3643a78bc9cbe993a38
rossPatton <me@rosspatton.com>
2014-08-30 22:35:45 -0700

    coffee and go versions of prob-6

--- prob-6-sum-square-diff.js
+++ prob-6-sum-square-diff.js
@@ -1,24 +1,27 @@
 /**
- * @reference https://projecteuler.net/problem=5
- * @description  2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
- * @problem What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
- * @solution 232792560
+ * @reference https://projecteuler.net/problem=6
+ * @description
+ *
+ * 	The sum of the squares of the first ten natural numbers is,
+ * 	12 + 22 + ... + 102 = 385
+ * 	The square of the sum of the first ten natural numbers is,
+ * 	(1 + 2 + ... + 10)2 = 552 = 3025
+ * 	Hence the difference between the sum of the squares of the first ten natural numbers and the square of the
+ * 	sum is 3025 − 385 = 2640.
+ *
+ * @problem Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ * @solution 25164150
 */
-
-// perfs
-const present = require('present');
 
 ;(function() {
 	'use strict';
-	let sum = 0,
+	let i = 1,
+			sum = 0,
 			sumSq = 0;
 
-	for (var i = 1; i <= 100; i++) {
+	for (i; i <= 100; i++) {
 		sum += i;
-	}
-
-	for (var j = 1; j <= 100; j++) {
-		sumSq += (j*j);
+		sumSq += (i*i);
 	}
 
 	console.log("The Sum Square Difference is: " + ( (sum*sum) - sumSq) );
