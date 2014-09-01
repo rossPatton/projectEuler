0a97b7b97f97cead0ce918ab2e926859f03fa50b
rossPatton <me@rosspatton.com>
2014-08-31 20:15:11 -0700

    count word frequency in string or array, return as key/val pairs

---
+++ prob-7-prime-num.js
@@ -0,0 +1,51 @@
+// /**
+//  * @reference https://projecteuler.net/problem=7
+//  * @description By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+//  * @problem What is the 10,0001st prime number?
+//  * @solution
+// */
+
+
+// i don't know why but i really hate prime nums, will come back to this
+
+// ;(function() {
+// 	'use strict';
+// 	let arr = [],
+// 			primeNotFound = true,
+// 			i = 1,
+// 			j = 1;
+
+// 	while (primeNotFound) {
+// 		if (i % 2 !== 0) {
+// 			console.log(i + ' is odd');
+// 			while (primeNotFound) {
+// 				console.log( % j)
+// 			}
+// 		}
+// 	// for (i; i <= 15; i++) {
+// 	// 	console.log(i)
+// 	// 	if (i % 2 !== 0) {
+// 	// 		for (j; j <= 15; j++) {
+// 	// 			console.log(j)
+// 	// 			if (i % j === 0) {
+// 	// 				// console.log(i)
+// 	// 				// console.log(j)
+// 	// 				console.log(i % j === 0)
+// 	// 				// break;
+// 	// 			} else {
+// 	// 				// console.log(i + ' prime');
+// 	// 				arr.push(i)
+// 	// 			}
+// 	// 		}
+// 	// 	}
+
+// 		i++;
+
+// 		if (i === 15) { primeNotFound = false; }
+// 	}
+
+// 	console.log(arr)
+
+// 	// console.log("The Sum Square Difference is: " + ( (sum*sum) - sumSq) );
+
+// }());
